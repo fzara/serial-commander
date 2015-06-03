faf9cfe29bfcf3e178f6eb25060ef70856016625
fzara <fzara@users.noreply.github.com>
2015-06-03 18:16:33 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # serial-commander
 Arduino serial response system
+
+Command pattern:
+12 34 5678
+
+12 = Function
+34 = Pin Number
+5678 = Value
+
+E.g. DW 15 1
--- Serial/Serial.ino
+++ Serial/Serial.ino
@@ -1,11 +1,30 @@
+	
+	
+	String cmd;
+	char cmdMode[2];
+	byte nextSpace, lastSpace, cmdPinNum;
+	int cmdValue;
+
 void setup() {
   // put your setup code here, to run once:
 	Serial.begin(9600);
-	String cmd[]="";
 	pinMode(13, OUTPUT);
 }
 
 void loop() {
-
+	if (Serial.available() > 0) {
+		cmd = Serial.readString();
+		extractCmd(cmd);
 	}
+}
 
+void extractCmd (String cmd) {
+	nextSpace = cmd.indexOf(' ');
+	cmd.substring(0,nextSpace).toCharArray(cmdMode,cmd.substring(0,nextSpace).length()); //extracts the charachters before the first space and converts them to char array.
+	lastSpace = nextSpace;
+	nextSpace = cmd.indexOf(' ',lastSpace);
+	cmdPinNum = cmd.substring(lastSpace+1,nextSpace).toInt();
+	lastSpace = nextSpace;
+	nextSpace = cmd.indexOf(' ',lastSpace);
+	cmdValue = cmd.substring(lastSpace+1,nextSpace).toInt();
+}
